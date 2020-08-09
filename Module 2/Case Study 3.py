538d1f2b8764ae1728d85e80b0d22f3942073d29
mannamab <mannamab@gmail.com>
2020-08-09 21:11:05 +0000

    case study 3

---
+++ Module 2/Case Study 3.py
@@ -0,0 +1,7 @@
+ReferenceID =  "f31b0(&d8h32"
+encrypted = ""  
+i = len(ReferenceID)  -  1   
+while  i >=  0 :   
+         encrypted = encrypted + ReferenceID[i]      
+         i  =  i  -  1     
+print(encrypted)
