1336086ae45b7175b869bcd43c10a8e5a8a52f65
mannamab <mannamab@gmail.com>
2020-08-14 15:17:06 +0000

    commit

---
+++ Module 4/Case Study 1 Q#1.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data= pd.read_csv("C:\\Users\\Administrator\\.vscode\\Module 4\\data_file.txt")
+print(data)
---
+++ Module 4/Untitled-1.py
@@ -0,0 +1,4 @@
+import os
+import pandas as pd
+data= pd.read_csv("C:\\Users\\Administrator\\.vscode\\Module 4\\PhysicsScoreTerm1.csv")
+print(data)
